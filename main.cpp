851b7e247aec16d8d602525510a6e4839304d878
Louis MENIGAULT <lomeniga@e1r1p6.clusters.42paris.fr>
2023-03-22 02:36:52 +0100

    split code in several function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <asm-generic/socket.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <sys/epoll.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <ostream>
 #include <sstream>
-std::ostream& operator<<(std::ostream& os, epoll_event& ev) {
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+struct client {
+  int fd;
+  int start;
+  int end;
+  std::string buf;
+};
+
+std::ostream &operator<<(std::ostream &os, epoll_event &ev) {
   if (ev.events & EPOLLHUP)
     os << "HUP";
   if (ev.events & EPOLLIN)
@@ -22,64 +30,80 @@ std::ostream& operator<<(std::ostream& os, epoll_event& ev) {
     os << "PRI";
   if (ev.events & EPOLLERR)
     os << "ERR";
-  os << ", " << ev.data.fd << "\n";
+  os << ", " << ev.data.ptr << "\n";
   return os;
 }
 
-struct client {
-  int fd;
-  int start;
-  int end;
-  std::string buf;
-};
-
+int pollfd;
+int tcp6_socket;
 client clients[1024];
 
-int main(int ac, char** av) {
-  int tcp6_socket = socket(AF_INET6, SOCK_STREAM, 0);
+void accept_client(epoll_event &ev) {
+  sockaddr_in6 addr = {};
+  socklen_t addrlen = sizeof(sockaddr_in6);
+  int fd = accept(tcp6_socket, NULL, 0);
+  std::cout << fd << " " << addr.sin6_port << '\n';
+  if (fd >= 0) {
+    clients[fd].fd = fd;
+    clients[fd].start = 0;
+    epoll_event event = {EPOLLIN, {.ptr = &clients[fd]}};
+    epoll_ctl(pollfd, EPOLL_CTL_ADD, fd, &event);
+    clients[fd].buf.reserve(512);
+  } else
+    std::cerr << "accept error\n";
+}
+
+void process_events(epoll_event &ev) {
+  // std::cout << ev;
+  if (ev.events & EPOLLIN) {
+    if (ev.data.fd == tcp6_socket) {
+      accept_client(ev);
+    } else {
+      client *c = (client *)ev.data.ptr;
+      char buf[512];
+      size_t len = read(c->fd, buf, 512);
+      if (len == 0) {
+        close(c->fd);
+        std::cerr << "close" << c->fd << '\n';
+        return;
+      }
+      c->buf.append(buf, len);
+      for (;;) {
+        size_t pos = c->buf.find("\n");
+        if (pos == std::string::npos)
+          break;
+        std::cerr << c->fd << ':' << c->buf.substr(0, pos) << '\n';
+        c->buf.erase(0, pos + 1);
+      }
+    }
+  } else if (ev.events & EPOLLOUT) {
+  } else {
+    std::cerr << ev;
+  }
+}
+
+int main(int ac, char **av) {
+  tcp6_socket = socket(AF_INET6, SOCK_STREAM, 0);
   int a = 1;
   setsockopt(tcp6_socket, SOL_SOCKET, SO_REUSEADDR, &a, sizeof(a));
   struct sockaddr_in6 addr = {AF_INET6};
   addr.sin6_port = htons(6667);
   addr.sin6_addr = in6addr_any;
-  int ret = bind(tcp6_socket, (sockaddr*)&addr, sizeof(addr));
+  int ret = bind(tcp6_socket, (sockaddr *)&addr, sizeof(addr));
   if (ret < 0)
     std::perror("ircserv");
   listen(tcp6_socket, 256);
   sockaddr_in6 peer_addr = {};
   socklen_t len = sizeof(peer_addr);
-  int pollfd = epoll_create(1);
+  pollfd = epoll_create(1);
   epoll_event event = {EPOLLIN, {.fd = tcp6_socket}};
   epoll_ctl(pollfd, EPOLL_CTL_ADD, tcp6_socket, &event);
   for (;;) {
     epoll_event events[64];
     int nev = epoll_wait(pollfd, events, 64, -1);
-    std::cout << "nev:" << nev << "\n";
+    std::cerr << "nev:" << nev << "\n";
     for (int i = 0; i < nev; i++) {
-      if (events[i].data.fd == tcp6_socket) {
-        int fd = accept(tcp6_socket, (sockaddr*)&peer_addr, &len);
-        clients[fd].fd = fd;
-        clients[fd].start = 0;
-        epoll_event event = {EPOLLIN, {.ptr = &clients[fd]}};
-        epoll_ctl(pollfd, EPOLL_CTL_ADD, fd, &event);
-        clients[fd].buf.reserve(512);
-      } else {
-        client* c = (client*)events[i].data.ptr;
-        char buf[512];
-        size_t len = read(c->fd, buf, 512);
-        if (len == 0) {
-          close(c->fd);
-          std::cout << "close" << c->fd << '\n';
-        }
-        c->buf.append(buf, len);
-        for (;;) {
-          size_t pos = c->buf.find("\n");
-          if (pos == std::string::npos)
-            break;
-          std::cout << c->fd << ':' << c->buf.substr(0, pos) << '\n';
-          c->buf.erase(0, pos + 1);
-        }
-      }
+      process_events(events[i]);
     }
   }
 }
