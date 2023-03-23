7bc6c205bbb6470c589d50503e1db7f246576cca
Camille CAMBIUM <ccambium@student.42.fr>
2023-03-23 13:36:19 +0100

    starting execution phase !

---
+++ handler.cpp
@@ -0,0 +1,25 @@
+#include "irc.hpp"
+#define COMMAND_COUNT 2
+void    _pass(std::list<std::string> * args)
+{
+    (void)args;
+}
+
+void    _user(std::list<std::string> * args)
+{
+    (void)args;
+}
+void    handler( std::list<std::string> * args)
+{
+    std::string commands[COMMAND_COUNT] = { "PASS", "USER" };
+    void (*handlers[COMMAND_COUNT])( std::list<std::string> * ) = { &_pass, &_user };
+    for (size_t i = 0; i < COMMAND_COUNT; i++)
+    {
+        if (!args->front().compare(commands[i]))
+        {
+            handlers[i](args);
+            return ;
+        }
+    }
+    std::cout << "COMMAND DO NOT EXIST (YET?)\n";
+}
