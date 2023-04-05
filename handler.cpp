4429a1be14e41ea9d8c3307a13fa8a717d9a5cba
Camille CAMBIUM <ccambium@student.42.fr>
2023-04-05 15:31:44 +0200

    PING MODE COMMANDS + format RPL_WELCOME

--- handler.cpp
+++ handler.cpp
@@ -6,14 +6,17 @@
 #include "ircserv.hpp"
 #include "utils.hpp"
 
-#define COMMAND_COUNT 7
+#define COMMAND_COUNT 9
 
 void pass( std::list<std::string>* args, client& c ) {
 	(void) args;
 	logger( "DEBUG", "PASS COMMAND" );
-	if ( args->front().compare( ircserv::getPassword() ) ) {
-		logger( "ERROR", "client %d : wrong password !", c.getFd() );
+	args->front().erase( args->front().length() - 1, 1 );
+	if ( args->front().compare( ircserv::getPassword() ) != 0 ) {
+		logger( "ERROR", "client %d : wrong password (%s)!", c.getFd(),
+		        args->front().c_str() );
 		close( c.getFd() );
+		return;
 	}
 	c.setHasGivenPassword( true );
 }
@@ -24,6 +27,10 @@ void user( std::list<std::string>* args, client& c ) {
 	        args->front().c_str() );
 	c.setHasGivenUser( true );
 	c.setUser( args->front() );
+	c.reply( format(
+	    ":ircserv.localhost 001 %s :Welcome to the FT_IRC "
+	    "Network, %s[!%s@foo.example.bar]\r\n",
+	    c.getUser().c_str(), c.getNick().c_str(), c.getUser().c_str() ) );
 }
 
 void privmsg( std::list<std::string>* args, client& c ) {
@@ -38,15 +45,12 @@ void join( std::list<std::string>* args, client& c ) {
 
 void nick( std::list<std::string>* args, client& c ) {
 	(void) args;
+	args->front().erase( args->front().length() - 1, 1 );
 	if ( args->empty() ) {
 		c.reply( "431\r\n" );
 	} else {
 		c.setNick( args->front() );
 		c.setHasGivenNick( true );
-		c.reply( format(
-		    ":ircserv.localhost 001 %s :Welcome to the FT_IRC "
-		    "Network, %s[!%s@foo.example.bar]\r\n",
-		    c.getUser().c_str(), c.getNick().c_str(), c.getUser().c_str() ) );
 		logger( "INFO", "client %d nickname is %s", c.getFd(),
 		        c.getNick().c_str() );
 	}
@@ -57,11 +61,37 @@ void capls( std::list<std::string>* args, client& c ) {
 	(void) args;
 }
 
+std::ostream& operator<<( std::ostream& os, std::list<std::string> arg ) {
+	std::list<std::string>::iterator it = arg.begin();
+	while ( it != arg.end() ) {
+		os << *it;
+		it++;
+	}
+	return os;
+}
+
+void pong( std::list<std::string>* args, client& c ) {
+	args->front().erase( args->back().length() - 1, 1 );
+	logger( "DEBUG", "PING from %s, token = %s", c.getNick().c_str(),
+	        args->back().c_str() );
+	c.reply( format( "PONG %s\r\n", args->back().c_str() ) );
+	std::cout << *args << "\n";
+}
+
+void mode( std::list<std::string>* args, client& c ) {
+	args->back().erase( args->back().length() - 1, 1 );
+	logger( "DEBUG", "user %s has now mode %s", c.getUser().c_str(),
+	        args->back().c_str() );
+	c.reply( format( "MODE :%s %s\r\n", c.getUser().c_str(),
+	                 args->back().c_str() ) );
+}
+
 void handler( std::list<std::string>* args, client& c ) {
-	std::string commands[COMMAND_COUNT] = { "PASS", "USER",  "NICK",
-	                                        "JOIN", "CAPLS", "CAP" };
-	void ( *handlers[COMMAND_COUNT] )( std::list<std::string>*, client& c ) = {
-	    &pass, &user, &nick, &join, &privmsg, &capls, &capls };
+	std::string commands[COMMAND_COUNT] = { "PASS", "USER",    "NICK",
+	                                        "JOIN", "PRIVMSG", "CAPLS",
+	                                        "CAP",  "PING",    "MODE" };
+	void ( *handlers[COMMAND_COUNT] )( std::list<std::string>*, client & c ) = {
+	    &pass, &user, &nick, &join, &privmsg, &capls, &capls, &pong, &mode };
 	for ( size_t i = 0; i < COMMAND_COUNT; i++ ) {
 		if ( !args->front().compare( commands[i] ) ) {
 			args->pop_front();
