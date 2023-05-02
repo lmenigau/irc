f479ca5432cb1adb034bd2829c26dbd611ab822f
Camille CAMBIUM <ccambium@e1r13p4.clusters.42paris.fr>
2023-05-02 14:46:46 +0200

    fixing little error on pass command

--- handler.cpp
+++ handler.cpp
@@ -10,6 +10,12 @@
 #define COMMAND_COUNT 12
 
 void pass( std::list<std::string>* args, Client& c ) {
+	if ( c.hasGivenPassword() ) {
+		c.reply(
+		    format( ":%s 462 %s :Unauthorized command (already registered)",
+		            ircserv::getServername().c_str(), c.getNick().c_str() ) );
+		return ;
+	}
 	if ( args->front().compare( ircserv::getPassword() ) != 0 ) {
 		logger( "ERROR", "client %d : wrong password (%s)!", c.getFd(),
 		        args->front().c_str() );
