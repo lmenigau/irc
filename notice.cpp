b9d8665a65c1b7459c9b17f1b22a5b39ee71cfae
Devsstudent <devsstudent777@gmail.com>
2023-05-04 23:03:04 +0200

    working on notice privmsg etc

--- notice.cpp
+++ notice.cpp
@@ -8,5 +8,22 @@
 #include "utils.hpp"
 
 void notice(std::list <std::string> *args, Client &c) {
-  
+	if ( args->empty() )
+		return;
+	std::string target = args->front();
+	//std::cout << target << std::endl;
+	while (args->size() != 1)
+	{
+	//	std::cout << "args :" << args->front() << std::endl;
+		if (isChannel(args->front()))
+		{
+			Channel* channel = find_channel( args->front() );
+			if (channel)
+				channel->notice(args, c);
+		}
+		else
+			notice_client(args, c);
+		args->pop_front();
+	}
+
 }
