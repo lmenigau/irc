7a45d6775dd55edfb9ea7d92ee57b9276994a505
Devsstudent <devsstudent777@gmail.com>
2023-05-04 15:13:52 +0200

    something happen on privmsg merging we didnt see, on patching ..

--- privmsg.cpp
+++ privmsg.cpp
@@ -7,54 +7,51 @@
 #include "ircserv.hpp"
 #include "utils.hpp"
 
-void	privmsg_client(std::list <std::string > * args, Client &c, const std::string &target)
+void	privmsg_client(std::list <std::string > * args, Client &c)
 {
 	for ( t_map_client::iterator it = ircserv::getClients().begin();
 	      									it != ircserv::getClients().end(); it++ ) {
 		if ( it->second.getNick() == args->front() ) {
 			args->front().append( "@" + ( it->second.getHostname() ) );
-			//std::cout << args->front() << " : " << args->back() << std::endl;
-			// std::cout << send((*it)->getFd(), buf, args->back().length(), 0)
-			// << std::endl;
 			it->second.reply(
 			    format( ":%s!~%s PRIVMSG %s: %s\r\n", c.getNick().c_str(),
 			            ( c.getUser() + "@" + c.getHostname() ).c_str(),
-			            target.c_str(), args->back().c_str() ) );
+			            args->front().c_str(), args->back().c_str() ) );
 			c.reply( format( ":%s!~%s PRIVMSG %s :%s\r\n", c.getNick().c_str(),
 			                 ( c.getUser() + "@" + c.getHostname() ).c_str(),
-			                 target.c_str(), args->back().c_str() ) );
-		} else if ( isChannel( target ) ) {
-			Channel* channel = find_channel( target );
+			                 args->front().c_str(), args->back().c_str() ) );
+		} else if ( isChannel( args->front() ) ) {
+			Channel* channel = find_channel( args->front() );
 			if ( channel ) {
 				channel->sendAll(
 				    format( ":%s!%s PRIVMSG %s :%s\r\n", c.getNick().c_str(),
 				            ( c.getUser() + "@" + c.getHostname() ).c_str(),
-				            target.c_str(), args->back().c_str() ),
+				            args->front().c_str(), args->back().c_str() ),
 				    c );
 			} else {
 				c.reply( format( ":%s 401 %s %s :No such nick/channel\r\n",
 				                 ircserv::getServername().c_str(),
-				                 c.getNick().c_str(), target.c_str() ) );
+				                 c.getNick().c_str(), args->front().c_str() ) );
 			}
 		} else {
 			c.reply( format( ":%s 401 %s %s :No such nick/channel\r\n",
 			                 ircserv::getServername().c_str(),
-			                 c.getNick().c_str(), target.c_str() ) );
+			                 c.getNick().c_str(), args->front().c_str() ) );
 		}
 	}
 }
 
-void	privmsg_channel(std::list <std::string> * args, Client &c, const std::string &target)
+void	privmsg_channel(std::list <std::string> * args, Client &c)
 {
 	t_map_channel           channels = ircserv::getChannels();
-	t_map_channel::iterator it = channels.find(target);
+	t_map_channel::iterator it = channels.find(args->front());
 
 	if (it == channels.end())
 	{
 		c.reply( format ("%s!~%s@%s 403 %s :No such channel\r\n", c.getNick().c_str(),
 															c.getUser().c_str(),
 															c.getHostname().c_str(),
-															target.c_str()));
+															args->front().c_str()));
 		return ;
 	}
 	else
@@ -64,12 +61,12 @@ void	privmsg_channel(std::list <std::string> * args, Client &c, const std::strin
 															cli_list++) {
 			std::string rep = format( ":%s!~%s@%s PRIVMSG %s: %s\r\n", c.getNick().c_str(),
 																	c.getUser().c_str(), c.getHostname().c_str(),
-																	target.c_str(), args->back().c_str());
+																	args->front().c_str(), args->back().c_str());
 			// std::cout << cli_list->second->getNick() << ": " << rep << std::endl;
 			if (*cli_list != &c)
 				( *cli_list )->reply( format( ":%s!~%s@%s PRIVMSG %s : %s\r\n", c.getNick().c_str(),
 																	c.getUser().c_str(), c.getHostname().c_str(),
-																	target.c_str(), args->back().c_str()));
+																	args->front().c_str(), args->back().c_str()));
 		}
 	}
 }
@@ -79,10 +76,17 @@ void privmsg( std::list<std::string>* args, Client &c) {
 		return;
 	std::string target = args->front();
 	//std::cout << target << std::endl;
-	if (isChannel(target))
-		privmsg_channel(args, c, target);
-	else
-		privmsg_client(args, c, target);
+		while (args->size() != 1)
+		{
+			std::cout << args->front() << std::endl;
+			if (isChannel(target))
+				privmsg_channel(args, c);
+			else
+				privmsg_client(args, c);
+			args->pop_front();
+		}
+
+
 	// void *buf = (const_cast<char *>(args->back().c_str()));
 
 	//logger( "INFO", "%s wants to send a message", c.getNick().c_str() );
