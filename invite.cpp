ffb4a18789b860b56cf910f611dd738c4a93220d
Devsstudent <devsstudent777@gmail.com>
2023-05-05 20:13:36 +0200

    save

---
+++ invite.cpp
@@ -0,0 +1,13 @@
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+#include <list>
+#include "client.hpp"
+#include "irc.hpp"
+#include "ircserv.hpp"
+#include "utils.hpp"
+
+void  invite( std::list<std::string> *args, Client &c)
+{
+  
+}
