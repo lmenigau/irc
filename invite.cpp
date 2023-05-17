6bbf1628dc8c4b1a914728f67907c5cf75c6ce6c
CAMBIUM camille <ccambium@gmail.com>
2023-05-17 05:31:05 +0200

    Fix : removing last format + clang-format

--- invite.cpp
+++ invite.cpp
@@ -5,35 +5,44 @@
 #include "client.hpp"
 #include "irc.hpp"
 #include "ircserv.hpp"
-#include "utils.hpp"
 #include "messageBuilder.hpp"
+#include "utils.hpp"
 
 void invite( std::list<std::string>* args, Client& c ) {
 	MessageBuilder mb;
-	
+
 	if ( args->size() == 1 || args->size() == 0 ) {
-		c.reply(mb << ircserv::getServername() << " 461 INVITE :Not enough parameters\r\n" );
+		c.reply( mb << ircserv::getServername()
+		            << " 461 INVITE :Not enough parameters\r\n" );
 		return;
 	}
 	Channel* channel_target = find_channel( args->back() );
 	if ( !channel_target )
-		return ( c.reply( mb << ircserv::getServername() << " 403 " << args->back() << ":No such channel\r\n" ) );
+		return ( c.reply( mb << ircserv::getServername() << " 403 "
+		                     << args->back() << ":No such channel\r\n" ) );
 	if ( !channel_target->findClients( c.getNick() ) )
-		return ( c.reply( mb << ircserv::getServername() << " 442 " << args->back() << ":You're not on that channel\r\n" ) );
+		return ( c.reply( mb << ircserv::getServername() << " 442 "
+		                     << args->back()
+		                     << ":You're not on that channel\r\n" ) );
 	// Priviliges part : depending on modes, not done for the moment
 	while ( args->size() != 1 ) {
 		mb.clear();
 		Client* client_target = find_client( args->front() );
 		if ( !client_target )
-			c.reply( mb << ircserv::getServername() << " 401 " << args->front() << ":No such nick\r\n" );
+			c.reply( mb << ircserv::getServername() << " 401 " << args->front()
+			            << ":No such nick\r\n" );
 		else if ( channel_target->findClients( args->front() ) )
-			c.reply( format(
-			    ":ircserv.localhost 443 %s %s :is already on channel\r\n",
-			    args->front().c_str(), args->back().c_str() ) );
+			c.reply( mb << ircserv::getServername() << " 443 " << args->front()
+			            << ' ' << args->back()
+			            << " :is already on channel\r\n" );
 		else {
-			c.reply( mb << ircserv::getServername() << " 341 " << c.getNick() << args->front() << args->back() );
+			c.reply( mb << ircserv::getServername() << " 341 " << c.getNick()
+			            << args->front() << args->back() );
 			mb.clear();
-			client_target->reply( mb << ":" << c.getNick() << "!~" << c.getHostname() << " INVITE " << args->front() << " " << args->back() << "\r\n" );
+			client_target->reply( mb << ":" << c.getNick() << "!~"
+			                         << c.getHostname() << " INVITE "
+			                         << args->front() << " " << args->back()
+			                         << "\r\n" );
 			channel_target->inviteUser( *client_target );
 		}
 		args->pop_front();
