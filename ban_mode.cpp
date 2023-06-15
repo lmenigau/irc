a6d7c1c73bbc7a181151cd2711574f71af1a6879
Louis MENIGAULT <lomeniga@bess-f2r1s19.clusters.42paris.fr>
2023-06-15 16:07:22 +0200

    typo and reformat

--- ban_mode.cpp
+++ ban_mode.cpp
@@ -1,6 +1,6 @@
-#include "channel.hpp"
 #include <iostream>
 #include <string>
+#include "channel.hpp"
 #include "client.hpp"
 #include "ircserv.hpp"
 #include "messageBuilder.hpp"
@@ -19,13 +19,15 @@ void Channel::reply_ban_list( Client& c ) {
 
 	for ( t_vector_client_ptr::iterator it = _banned.begin();
 	      it != _banned.end(); it++ ) {
-	mb << ":ircserv.locahost 367 " << c.getNick() << " " << _name;
-		mb << " " << ( *it )->getNick() << "!" << "*@*";
-	mb << " " << c.getNick() << "!~" << c.getUser() << "@" << c.getHostname() << "\r\n";
-	sendAll( mb );
+		mb << ":ircserv.locahost 367 " << c.getNick() << " " << _name;
+		mb << " " << ( *it )->getNick() << "!"
+		   << "*@*";
+		mb << " " << c.getNick() << "!~" << c.getUser() << "@"
+		   << c.getHostname() << "\r\n";
+		sendAll( mb );
 	}
 	(void) c;
-	sendAll(":irvserv.localhost 368 :End of channel ban list\r\n");
+	sendAll( ":irvserv.localhost 368 :End of channel ban list\r\n" );
 }
 
 void Channel::m_ban( Client& c, std::string args, t_ope operation ) {
@@ -36,8 +38,7 @@ void Channel::m_ban( Client& c, std::string args, t_ope operation ) {
 	MessageBuilder mb;
 	size_t         i = 0;
 
-	while (i != args.size())
-	{
+	while ( i != args.size() ) {
 		target = getTarget( i, args );
 		if ( target.empty() )
 			continue;
@@ -51,24 +52,24 @@ void Channel::m_ban( Client& c, std::string args, t_ope operation ) {
 		    std::find( _banned.begin(), _banned.end(), client );
 		if ( operation == ADD ) {
 			sendAll( mb << ':' << c.getNick() << '!' << c.getUser() << '@'
-			            << c.getHostname() << " MODE " << _name << " +b " 
-						<< target + "!*@*\r\n");
+			            << c.getHostname() << " MODE " << _name << " +b "
+			            << target + "!*@*\r\n" );
 			if ( it == _banned.end() )
 				_banned.push_back( client );
 			reply_ban_list( c );
-			addModes("b");
-	} else {
+			addModes( "b" );
+		} else {
 			if ( it == _banned.end() ) {
 				c.reply( mb << ":" << ircserv::getServername() << " 441 "
 				            << target << " " << _name
-				            << " :They aren't not banned on that channel\r\n" );
+				            << " :They aren't banned on that channel\r\n" );
 				continue;
 			}
 			_banned.erase( it );
-			removeModes("b");
+			removeModes( "b" );
 			sendAll( mb << ':' << c.getNick() << '!' << c.getUser() << '@'
-			            << c.getHostname() << " MODE " << _name << " +b " 
-						<< target + "!*@*\r\n");
+			            << c.getHostname() << " MODE " << _name << " +b "
+			            << target + "!*@*\r\n" );
 			reply_ban_list( c );
 		}
 	}
